8a1a6260bb22efeb1efd2a263c6e044851f31c71
Hridesh Srivastava <hrideshsrivastavaa@outlook.com>
2024-09-12 16:23:12 +0530

    add some changes

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import Freecurrencyapi from '@everapi/freecurrencyapi-js';
 
+//follow the code below:
 const freecurrencyapi = new Freecurrencyapi('fca_live_PzAClKWRtHEmVW6hO1xipGpan6v86yxQ0Un5eA2v');
 
 export default async function convertCurrency(fromCurrency , toCurrency , units){
